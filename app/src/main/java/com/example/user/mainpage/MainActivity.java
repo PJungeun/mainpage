9065dbfb528a045b77f8029b38a3c213d0e3bcc0
oh <dhforb1234@naver.com>
2020-05-10 23:29:47 +0900

    mainpage_03

--- app/src/main/java/com/example/user/mainpage/MainActivity.java
+++ app/src/main/java/com/example/user/mainpage/MainActivity.java
@@ -1,13 +1,52 @@
 package com.example.user.mainpage;
 
+import android.support.annotation.NonNull;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;//나중에 지울것
 
 public class MainActivity extends AppCompatActivity {
+    private DrawerLayout drawerLayout;
+    private View drawerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void share_btnClick(View view) {
+        Toast.makeText(getApplicationContext(),"공유버튼테스트",Toast.LENGTH_LONG).show();
+
+    }
+
+    public void camera_btnClick(View view) {
+        Toast.makeText(getApplicationContext(),"카메라버튼테스트",Toast.LENGTH_LONG).show();
+
+    }
+
+    //Navigation 기능 부분
+    DrawerLayout.DrawerListener listener = new DrawerLayout.DrawerListener() {
+        @Override
+        public void onDrawerSlide(@NonNull View drawerView, float slideOffset) {
+
+        }
+
+        @Override
+        public void onDrawerOpened(@NonNull View drawerView) {
+
+        }
+
+        @Override
+        public void onDrawerClosed(@NonNull View drawerView) {
+
+        }
+
+        @Override
+        public void onDrawerStateChanged(int newState) {
+
+        }
+    };
 }
